432c72167b9742fbc69610e461b2ae44ef18d5c9
ibrobabs <babskolawole@gmail.com>
2017-12-31 22:21:50 -0700

    update

--- clinic1/wsgi.py
+++ clinic1/wsgi.py
@@ -4,6 +4,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "clinic1.settings.dev")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "clinic1.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "clinic1.settings")
 
 application = get_wsgi_application()
